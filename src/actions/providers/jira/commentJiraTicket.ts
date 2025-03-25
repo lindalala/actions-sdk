a22974ee563b8e3ce0851bf528c5985ad8acd08e
Linda Li <linda@vulcancollective.io>
2025-03-25 01:13:57 -0700

    add comment jira action

---
+++ src/actions/providers/jira/commentJiraTicket.ts
@@ -0,0 +1,56 @@
+import { AxiosError } from "axios";
+import {
+  AuthParamsType,
+  jiraCommentJiraTicketFunction,
+  jiraCommentJiraTicketOutputType,
+  jiraCommentJiraTicketParamsType,
+} from "../../autogen/types";
+import { axiosClient } from "../../util/axiosClient";
+
+const CommentJiraTicket: jiraCommentJiraTicketFunction = async ({
+  params,
+  authParams,
+}: {
+  params: jiraCommentJiraTicketParamsType;
+  authParams: AuthParamsType;
+}): Promise<jiraCommentJiraTicketOutputType> => {
+  const { authToken, cloudId, baseUrl } = authParams;
+
+  if (!cloudId || !params?.issueId) {
+    throw new Error("Cloud ID and Issue ID are required to comment on a Jira ticket");
+  }
+  const apiUrl = `https://api.atlassian.com/ex/jira/${cloudId}/rest/api/3/issue/${params.issueId}/comment`;
+  const response = await axiosClient.post(
+    apiUrl,
+    {
+      body: {
+        type: "doc",
+        version: 1,
+        content: [
+          {
+            type: "paragraph",
+            content: [
+              {
+                type: "text",
+                text: params.comment,
+              },
+            ],
+          },
+        ],
+      },
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${authToken}`,
+        Accept: "application/json",
+        "Content-Type": "application/json",
+      },
+    },
+  );
+
+  return {
+    commentUrl: `${baseUrl}/browse/${params.issueId}?focusedCommentId=${response.data.id}`,
+  };
+};
+
+export default CommentJiraTicket;
