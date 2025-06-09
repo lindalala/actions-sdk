798fc4afb24c8238281582ccde5c1d160e841f71
Betsy <8404974+betsy@users.noreply.github.com>
2025-06-09 15:42:39 -0400

    test if file extension results in different error (#188)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Do not use "@/" in this file because it won't generate declaration files properly
 
-export { runAction, getActions } from "./app";
+export { runAction, getActions } from "./app.js";
 export { ActionMapper } from "./actions/actionMapper";
 export { ActionTemplate } from "./actions/parse";
 
