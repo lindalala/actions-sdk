8cf56e86d339e9958b33a6f8673a189d657e2234
Richard Liu <richard@credal.ai>
2025-09-08 16:27:42 -0400

    Actually export helper function (#351)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 // Do not use "@/" in this file because it won't generate declaration files properly
 
-export { runAction, getActions } from "./app.js";
+export { runAction, getActions, getActionByProviderAndName } from "./app.js";
 export { ActionMapper } from "./actions/actionMapper.js";
 export { ActionTemplate } from "./actions/parse.js";
 
